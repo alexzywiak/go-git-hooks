e0ce93b504126ff22d0aaa6e5a56dd5232edc1ff
Alex Zywiak <alex.zywiak@aspenmesh.io>
2020-03-05 13:08:59 -0700

    filepath

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+	filepath.Walk(".", func(path string, f os.FileInfo, err Error) error {
+		fmt.Println(path)
+	})
 	fmt.Printf(context.Background().Value())
 }
